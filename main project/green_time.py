2a11d411c5a756c2337286b19922e1480d339dd7
Chaerin Ryu <dmsquf1009@dgu.ac.kr>
2024-05-16 19:07:31 +0900

    :memo: 긴급차량 우선신호 함수 파일 옮김

---
+++ main project/green_time.py
@@ -0,0 +1,13 @@
+import traci as tr
+
+def green_time(lane_id, veh_id, edge_id):
+    # 대기 차량 소거시간 계산
+    qc = tr.edge.getLastStepHaltingNumber(edge_id) / tr.edge.getLaneNumber(edge_id) * 2
+
+    # 긴급차량 통과시간 계산
+    t = (tr.lane.getLength(lane_id) - (qc * tr.vehicle.getSpeed(veh_id))) / tr.vehicle.getSpeed(veh_id)
+
+    g = qc + t
+    print(g)
+
+    return g
