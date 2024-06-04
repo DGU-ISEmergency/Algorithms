edbcebeaa5cb12b57a2e8e43fcc55c6a171857ba
lsjmc0224 <lsjmc0224@dgu.edu>
2024-06-04 19:43:12 +0900

    :memo: Fix : qc 계산과정에서 edge 개수 5개로 잘못 계산하는 오류 해결
    추가로 값들 계산값 나오도록 설정

--- main project/green_time.py
+++ main project/green_time.py
@@ -2,11 +2,20 @@ import traci as tr
 
 def green_time(lane_id, veh_id, edge_id):
     # 대기 차량 소거시간 계산
-    qc = tr.edge.getLastStepHaltingNumber(edge_id) / tr.edge.getLaneNumber(edge_id) * 2
+    qc = tr.edge.getLastStepHaltingNumber(edge_id) / (tr.edge.getLaneNumber(edge_id) - 1) * 2
 
     # 긴급차량 통과시간 계산
+    print("=============================================")
+    print(f"긴급차량 속도: {tr.vehicle.getSpeed(veh_id)}")
+    print(f"edge_id: {edge_id}")
+    print(f"lane_id: {lane_id}")
+    print(f"대기 차량 수 : {tr.edge.getLastStepHaltingNumber(edge_id)}")
+    print(f"차선 수: {tr.edge.getLaneNumber(edge_id)}")
+    print(f"차선 길이: {tr.lane.getLength(lane_id)}")
+    print(f"qc: {qc}")
     t = (tr.lane.getLength(lane_id) - (qc * tr.vehicle.getSpeed(veh_id))) / tr.vehicle.getSpeed(veh_id)
-
     g = qc + t
+    print(f"g: {g}")
+    print("=============================================")
 
     return g
