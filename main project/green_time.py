2e62a1d1a5bdff8c6a35c66ff7a271ffdb494f3a
Chaerin Ryu <dmsquf1009@dgu.ac.kr>
2024-09-20 13:54:06 +0900

    ⚡️ Feat: 기존 신호 제어 시 원래 phase로 돌아감

--- main project/green_time.py
+++ main project/green_time.py
@@ -1,12 +1,11 @@
 import traci as tr
 
 def green_time(lane_id, veh_id, edge_id):
+    # 긴급차량 통과시간 계산
+    t = (tr.lane.getLength(lane_id) - (qc * tr.vehicle.getSpeed(veh_id))) / max(tr.vehicle.getSpeed(veh_id), 1)
+    
     # 대기 차량 소거시간 계산
     qc = tr.edge.getLastStepHaltingNumber(edge_id) / (tr.edge.getLaneNumber(edge_id) - 1) * 2
-
-    # 긴급차량 통과시간 계산
-
-    t = (tr.lane.getLength(lane_id) - (qc * tr.vehicle.getSpeed(veh_id))) / max(tr.vehicle.getSpeed(veh_id), 1)
     g = qc + t
 
     return g
